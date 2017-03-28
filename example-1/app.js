4bd9815131d597fd219c9f5a934bc216029cb99c
zhanliwen <li_man@live.co.uk>
2017-03-28 07:43:41 +0100

    example 1

--- example-1/app.js
+++ example-1/app.js
@@ -46,6 +46,7 @@ function onlyName(entity) {
 
 function extractCity (params, callback) {
     params.language = 'english';
+	console.log(params);
     alchemyLanguage.entities(params, function(err, response) {
       if (err) {
         callback(err);
@@ -61,49 +62,49 @@ function extractCity (params, callback) {
 
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
-  var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
-	
-  };
-  // Send the input to the conversation service
-  
-   var message = extend({ input: {text: req.text} },req);
-    var input = message.text ? { text: message.text } : message.input;
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      return res.status(err.code || 500).json(err);
-    }
-	var result = updateMessage(payload, data)
-	
-	var context = {};
-	data.context = context;
-/* 	return extractCity(input,function(city){
-		if (city) {
-			if (!context.city) {
-				context.city = city
+	var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+	if (!workspace || workspace === '<workspace-id>') {
+		return res.json({
+			'output': {
+				'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
 			}
-			context.city.alternate_name = city.name;
-			
+		});
+	}
+
+	var payload = {
+		workspace_id: workspace,
+		context: req.body.context || {},
+		input: req.body.input || {}
+	};
+  
+	// Send the input to the conversation service
+  
+	
+	
+	  // Send the input to the conversation service
+	var extractedCity
+	var alternate
+	var message = extend({ input: {text: payload.text} }, payload);
+	var input = payload.text ? { text: payload.text } : payload.input;
+	console.log(input);
+	if(input.text !={}){
+		extractCity (input, function(city){
+			if(city){
+				extractCity = city;
+				console.log(city)
+			}
+			alternate = city.name
+		})
+	}
+	
+	return conversation.message(payload, function(err, data) {
+		if (err) {
+		  return res.status(err.code || 500).json(err);
 		}
-	}) */
-	
-	
-	
-	
-	console.log("\n");
-	console.log(result);
-    return res.json(result);
-  });
+		var result = updateMessage(payload, data)
+		return res.json(result);
+	});
+
 });
 
 /**
@@ -119,12 +120,20 @@ function updateMessage(input, response) {
   } else {
     return response;
   }
-
-  var context = {};
-  response.context = context;
   if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-	
+     var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
   }
   response.output.text = responseText;
   return response;
--- example-1/server.js
+++ example-1/server.js
@@ -1,16 +1,34 @@
 'use strict';
 
 require('dotenv').config({silent: true});
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var rateLimit = require('express-rate-limit');
+var helmet = require('helmet');
+var http = require('http').Server(app);
 
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 var debug = require('debug')('bot:server');
 
+// Deployment tracking
+require('cf-deployment-tracker-client').track();
 
+// configure express
+app.use(helmet());
+app.use('/api/', rateLimit({
+  windowMs: 60 * 1000, // seconds
+  delayMs: 0,
+  max: 15
+}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 var server = require('./app');
-
 
 server.listen(port, function() {
   // eslint-disable-next-line
   console.log('Server running on port: %d', port);
 });
+
+module.exports = http
